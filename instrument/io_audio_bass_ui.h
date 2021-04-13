6c0d6066f96d079b6abd276b94e232283425fbaf
alex <alexandre.piel@gmail.com>
2021-04-13 22:29:38 +0200

    cleanup

--- instrument/io_audio_bass_ui.h
+++ instrument/io_audio_bass_ui.h
@@ -31,44 +31,6 @@ class IO_AudioBassUI : public IO_AudioBass {
                   filterResonance);
         d->printf("Dist %d range %d\n", (int)distortion.amount,
                   (int)distortion.range);
-
-        // if (mcMode) {
-        //     if (modulationOn) {
-        //         addToCursor(d, 0, 4);
-        //         d->println("Modulation level");
-        //         d->printf("%.2f|%.2f|%.2f|%.2f\n", modLevel[0], modLevel[1],
-        //                   modLevel[2], modLevel[3]);
-        //         d->printf("%.2f|%.2f|%.2f|%.2f\n", modLevel[4], modLevel[5],
-        //                   modLevel[6], modLevel[7]);
-
-        //         addToCursor(d, 0, 4);
-        //         d->println("Modulation ms");
-        //         d->printf("%d|%d|%d|%d\n", (int)modMs[0], (int)modMs[1],
-        //                   (int)modMs[2], (int)modMs[3]);
-        //         d->printf("%d|%d|%d|%d\n", (int)modMs[4], (int)modMs[5],
-        //                   (int)modMs[6], (int)modMs[7]);
-        //     } else {
-        //         d->println("\nNo modulation");
-        //     }
-        // } else {
-        //     d->printf("%.1fHz %d%%\n", waveform.frequency,
-        //               (int)(waveform.amplitude * 100));
-
-        //     d->printf("Start %d EG %d|%d\n", waveform.getStart(),
-        //     (int)attackMs,
-        //               (int)decayMs);
-
-        //     addToCursor(d, 0, 4);
-        //     d->printf("%s %.1fHz %.1f\n", getFilter(currentFilter),
-        //               filterFrequency, filterResonance);
-
-        //     d->printf("Dist %d range %d\n", (int)distortion.amount,
-        //               (int)distortion.range);
-        //     d->printf("Bitcrusher %d\n", (int)xcrushBits);
-
-        //     d->printf("%s %s\n", modulationOn ? "ModON" : "ModOFF",
-        //               rectifierOn ? "RectON" : "RectOFF");
-        // }
     }
 
     void noteOnHandler(byte channel, byte note, byte velocity) {
