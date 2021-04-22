89433dd7fef4717f3955a1328a8949fb3c861b9f
alex <alexandre.piel@gmail.com>
2021-04-22 21:46:59 +0200

    mixer

--- io_audio.h
+++ io_audio.h
@@ -7,7 +7,12 @@
 #include "io_instrument.h"
 
 AudioOutputMQS audioOut;
-AudioConnection patchCordSynth(synth[SYNTH_0], audioOut);
+AudioMixer4 mixerSynth;
+AudioConnection patchCordMixerSynth(mixerSynth, audioOut);
+AudioConnection patchCordSynth0(synth[SYNTH_0], mixerSynth);
+AudioConnection patchCordSynth1(synth[SYNTH_1], 0, mixerSynth, 1);
+AudioConnection patchCordSynth2(synth[SYNTH_2], 0, mixerSynth, 2);
+AudioConnection patchCordSynth3(synth[SYNTH_3], 0, mixerSynth, 3);
 
 void audioInit() {
     AudioMemory(25);
